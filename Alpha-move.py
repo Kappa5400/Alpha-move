db1525d5bcc206a1b2466055c12d9a8f2ad55f3a
Kappa5400 <kappa6708@gmail.com>
2025-02-20 14:41:40 +0000

    fun

--- Alpha-move.py
+++ Alpha-move.py
@@ -3,27 +3,42 @@ import requests
 
 board = chess.Board()
 
-board.legal_moves
-print(board.legal_moves)
-
 
 class engine():
-
     def alpha_move():
         moves =[]
-        moves= board.legal_moves
+        moves= list(board.legal_moves)
         if len(moves) % 2 == 0:
-            half = len(moves) / 2
-            alpha = board.legal_moves[half]
+            alpha = len(moves) / 2
+            
         else:
-            half = (len(moves) + 1) / 2
-            alpha = board.legal_moves[half]
+            moveandone = (len(moves) + 1)
+            alpha = moveandone / 2
 
-        return alpha
+        alpha = int(alpha)
+        print("Alpha:{move[alpha]}")
+        board.push_san(f"{moves[alpha]}")
+       
 
-        def player_move():
-            return board.legal_moves[0]
+        return
 
+    def player_move():
+        p_move = input("What is your move?: ")
+        moves = []
+        moves = list(board.legal_moves)
+        print(moves[0])
+        board.push_san(f"{moves[0]}")
+        
+        return 
 
+        
+
+while board.is_game_over() == 0:
+    engine.player_move()
+    print(board)
+    engine.alpha_move()
+    print(board)
+    
+        
 
 
